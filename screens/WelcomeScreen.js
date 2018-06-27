3cfdc1f5df3aa5ba55e8ce780785fc8d4c990845
Giulio Gatto <giuliogatto@gmail.com>
2018-06-27 10:54:47 +0200

    fetch and show

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, View, StyleSheet, Text} from 'react-native'
+import {Button, View, StyleSheet, Text, FlatList, Image , ScrollView} from 'react-native'
 
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
@@ -9,17 +9,46 @@ export default class WelcomeScreen extends React.Component {
       <Ionicons name={`ios-options${focused ? '' : '-outline'}`} size={25} color={tintColor} />
     ),
   }
+
+  state = {
+    data: [],
+  }
+
   componentDidMount(){
     fetch('https://5b3348f7d167760014c26437.mockapi.io/api/v1/items')  
     .then(response => response.json())
     .then(data => {
       console.log(data)
+      this.setState({data:data})
     });
+  }
+
+
+  clicked(id){
+    console.log(id)
   }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Welcome Screen.</Text>
+        <ScrollView>
+          <FlatList
+            data={this.state.data}
+            keyExtractor={(item, index) => item.id}
+            renderItem={({item}) => 
+            <View>
+              
+              <Button onPress={() => this.clicked(item.id)}
+              title={item.id + ' ' + item.name}
+              color="#000000"
+              />
+             
+            </View>
+            }
+          />
+          
+         
+      </ScrollView>
       </View>
     )
   }
